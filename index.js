172fff581c6e0e6cc85fd6999e567eba605a6bfe
nur.rahmat <nur.rahmat@aksaramaya.com>
2022-05-29 18:44:32 +0700

    Done Task Challange JS Structure Data - Revision 03

--- index.js
+++ index.js
@@ -8,15 +8,24 @@ const genderMonica = "Female"
 const ageMonica = 17;
 const emailMonica = "monica@dingdong.com";
 const favoriteColorMonica = ["Yellow", "Pink", "White", "Purple"];
-let setFavoriteColorMonica = new Set(favoriteColorMonica); 
 const isHavePetMonica = "Yes"
 const educationMonica = [
     { name: "SD 01", city: "Jakarta", graduate: "2016" },
     { name: "SMP 02", city: "Jakarta", graduate: "2019" },
     { name: "SMA 03", city: "Tangerang" }
 ];
-const favoriteRestoMonica = ["Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento", "Eggy", "Padang", "Tteok", "Sushi", "Sushi"]; 
-let setFavoriteRestoMonica = new Set(favoriteColorMonica); 
+const favoriteRestoMonica = ["Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento", "Eggy", "Padang", "Tteok", "Sushi", "Sushi"];  
+
+const firstUser = {
+    name: nameMonica,
+    gender: genderMonica,
+    age: ageMonica,
+    email: emailMonica,
+    favoriteColor: favoriteColorMonica,
+    isHavePet: isHavePetMonica,
+    education: educationMonica,
+    favoriteRestaurant: favoriteRestoMonica 
+};
 
 /** Data Wendy */
 const nameWendy = "Wendy"
@@ -24,7 +33,6 @@ const genderWendy = "Male"
 const ageWendy = 23;
 const emailWendy = "wendy@dingdong.com";
 const favoriteColorWendy = ["Blue", "Black", "Grey"];
-let setFavoriteColorWendy = new Set(favoriteColorWendy); 
 const isHavePetWendy = "No"
 const educationWendy = [
     { name: "SD 02", city: "Jakarta", graduate: "2010" },
@@ -33,28 +41,16 @@ const educationWendy = [
     { name: "Universitas Maju", city: "Tangerang" }
 ];
 const favoriteRestoWendy = ["Tempura", "Bento", "Sushi", "Pancake", "Padang", "Katsu", "Geprek", "Pancake", "Eggy"];   
-let setFavoriteRestoWendy = new Set(favoriteRestoWendy); 
-
-const firstUser = {
-    name: nameMonica,
-    gender: genderMonica,
-    age: ageMonica,
-    email: emailMonica,
-    favoriteColor: [...setFavoriteColorMonica],
-    isHavePet: isHavePetMonica,
-    education: educationMonica,
-    favoriteResto: [...setFavoriteRestoMonica] 
-};
 
 const secondUser = {
     name: nameWendy,
     gender: genderWendy,
     age: ageWendy,
     email: emailWendy,
-    favoriteColor: [...setFavoriteColorWendy] ,
+    favoriteColor: favoriteColorWendy,
     isHavePet: isHavePetWendy,
     education: educationWendy,
-    favoriteResto: [...setFavoriteRestoWendy] 
+    favoriteRestaurant: favoriteRestoWendy 
 };
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
